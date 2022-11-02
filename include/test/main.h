53c93c48c37b1bc423fd32e4db22c076bebe8704
Matthew Crowell <78377986+Cahlien@users.noreply.github.com>
2022-11-01 20:19:07 -0500

    update configuration

---
+++ include/test/main.h
@@ -0,0 +1,13 @@
+//
+// Created by Matthew Crowell on 10/31/2022.
+//
+
+#ifndef UNITTESTS_MAIN_H
+#define UNITTESTS_MAIN_H
+
+#include <QQmlExtensionPlugin>
+#include <QtQuickTest>
+
+Q_IMPORT_QML_PLUGIN(dev_crowell_UIPlugin)
+
+#endif //UNITTESTS_MAIN_H
--- test/ui/pages/main/src/main.cpp
+++ test/ui/pages/main/src/main.cpp
@@ -1,7 +1,7 @@
 //
 // Created by Matthew Crowell on 10/3/2022.
 //
-#include <QQmlExtensionPlugin>
-Q_IMPORT_QML_PLUGIN(dev_crowell_UIPlugin)
-#include <QtQuickTest>
+
+#include "test/main.h"
+
 QUICK_TEST_MAIN(MainPageTests)
